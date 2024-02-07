67c4823035a68ae7ffb0233f1011fcfd3ef117e3
vitorhes <vitorhes@outlook.com>
2024-02-06 21:17:21 -0300

    adjunsting

--- thread.py
+++ thread.py
@@ -4,16 +4,16 @@ class MethodMonitor:
     def __call__(self, *params_to_collect):
         def decorator(method):
             def wrapper(instance, *args, **kwargs):
-                arguments = {param: value for param, value in zip(params_to_collect, args)}
-                arguments.update(kwargs)
-                
-                payload = {"method": method.__name__, "arguments": arguments}
-                thread= threading.Thread(
-                    target=PayloadSender.send_http_request,
-                    args=(payload,), daemon=True)
-                thread.start()
-                #thread.join()  # Wait for the thread to finish
-                return method(instance, *args, **kwargs)
+                try:
+                    arguments = {param: value for param, value in zip(params_to_collect, args)}
+                    arguments.update(kwargs)
+                    
+                    payload = {"method": method.__name__, "arguments": arguments}
+                    thread = threading.Thread(target=PayloadSender.send_http_request, args=(payload,))
+                    thread.start()
+                    return method(instance, *args, **kwargs)
+                except Exception as e:
+                    return method(instance, *args, **kwargs)
 
             return wrapper
 
@@ -31,11 +31,9 @@ class CatalogConnector:
 
     @metadata_updater("database", "table")
     def get_table_to_df(self, database, table, additional_param):
-        current_thread = threading.current_thread()
-        print(f"get from thread {current_thread.name} ({current_thread.ident})")
+        print("hello")
    
     @metadata_updater("df", "database", "table")
     def put_df_to_table(self, df, database, table):
-        current_thread = threading.current_thread()
-        print(f"put from thread {current_thread.name} ({current_thread.ident})")
+        print("putt")
 
