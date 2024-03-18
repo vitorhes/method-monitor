f0499d41a3f1cfb5d4af5720c4ab2a6224235f4a
vitorhes <vitorhes@outlook.com>
2024-03-18 12:50:46 -0300

    fiz get month

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
-from catalogo import CatalogConnector
-# Example usage:
-conn = CatalogConnector()
-
-#conn.get_table_to_df(database="db", table="tabela1", additional_param="extra_info")
-conn.get_table_to_df("db","table",additional_param="as")
-
-# This will print the simulated HTTP request with the payload
-
-#conn.put_df_to_table("db","tabela1","extra_info")
-#conn.put_df_to_table(df="df2",database="db", table="tabela1")
+from datetime import datetime
+import calendar
+from typing import Tuple
+def __get_month_range(month: int) -> Tuple[str, str]:
+    # Get the current year and calculate the start and end dates for the given month
+    current_year = datetime.now().year
+    num_days_in_month = calendar.monthrange(current_year, month)[1]
+    start_date = datetime(current_year, month, 1).isoformat()
+    end_date = datetime(current_year, month, num_days_in_month).isoformat()
+    return start_date, end_date
+x,y = __get_month_range(3)
+print(x,y)
